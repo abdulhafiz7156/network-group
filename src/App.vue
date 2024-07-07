e388cb3323e87ee87cc43a69cfbdff9d8388a1bf
abdulhafiz7156 <xxafiz69@gmail.com>
2024-07-07 10:50:21 +0500

    finished first and second blocks without media, third block started

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,95 @@
-<script setup>
+<script>
 </script>
 
 <template>
-
+    <main>
+      <div class="container">
+        <section class="first-block">
+          <div class="first-block-div df">
+            <div class="first-block__text-div ">
+              <div class="first-block__text-div__logo df_ai ">
+                <img src="./assets/img/logo.png" alt="">
+                <p>Network Group</p>
+              </div>
+              <div class="first-block__text-div__texts">
+                <h1>Превратите возможности в планы</h1>
+                <p>Таким образом реализация намеченных плановых заданий в значительной степени обуславливает создание систем массового участия.</p>
+                <button>Открытые вакансии [12]</button>
+              </div>
+            </div>
+            <div class="first-block__img-div">
+              <img src="./assets/img/bg.png" alt="Background image">
+            </div>
+          </div>
+        </section>
+        <section class="second-block container__80">
+          <div class="blocks__title">
+            <h3>Наши продукты_</h3>
+          </div>
+          <div class="second-block__cards df">
+            <div class="second-block__card green-card">
+              <div class="card__mini-div card__mini-div--green">Product • 001</div>
+              <div class="second-block__card__texts-img df">
+                <div class="second-block__card__texts">
+                  <img src="./assets/img/ng-logo.png" alt="NG logo">
+                  <h2>Network Graphics</h2>
+                  <p>Таким образом реализация намеченных плановых заданий в значительной степени обуславливает создание.</p>
+                </div>
+                <div class="second-block__card__img df">
+                  <img src="./assets/img/ng-vector.png" alt="NG Vector">
+                </div>
+              </div>
+            </div>
+            <div class="second-block__card white-card">
+              <div class="card__mini-div card__mini-div--white">Product • 002</div>
+              <div class="second-block__card__texts-img df">
+                <div class="second-block__card__texts">
+                  <img src="./assets/img/meta-logo.png" alt="NG logo">
+                  <h2>Meta Network</h2>
+                  <p>Таким образом реализация намеченных плановых заданий в значительной степени обуславливает создание.</p>
+                </div>
+                <div class="second-block__card__img df">
+                  <img src="./assets/img/meta-vector.png" alt="Meta Vector">
+                </div>
+              </div>
+            </div>
+            <div class="second-block__card yellow-card">
+              <div class="card__mini-div card__mini-div--yellow">Product • 003</div>
+              <div class="second-block__card__texts-img df">
+                <div class="second-block__card__texts">
+                  <img src="./assets/img/startapp.png" alt="StartApp logo">
+                  <h2>StartApp</h2>
+                  <p>Таким образом реализация намеченных плановых заданий в значительной степени обуславливает создание.</p>
+                </div>
+                <div class="second-block__card__img df">
+                  <img src="./assets/img/startapp-vector.png" alt="StartApp Vector">
+                </div>
+              </div>
+            </div>
+            <div class="second-block__card violet-card">
+              <div class="card__mini-div card__mini-div--violet">Product • 004</div>
+              <div class="second-block__card__texts-img df">
+                <div class="second-block__card__texts">
+                  <img src="./assets/img/startapp.png" alt="StartApp logo">
+                  <h2>Зарубежный проект</h2>
+                  <p>Таким образом реализация намеченных плановых заданий в значительной степени обуславливает создание.</p>
+                </div>
+                <div class="second-block__card__img df">
+                  <img src="./assets/img/foreign-vector.png" alt="Foreign Vector">
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="third-block container__80">
+          <div class="blocks__title">
+            <h3>Таймлайн_</h3>
+          </div>
+        </section>
+        <section></section>
+        <section></section>
+      </div>
+    </main>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+import './assets/css/color.css'
+import './assets/css/classes.css'
+import './assets/css/fonts.css'
+import './assets/css/main.css'
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
