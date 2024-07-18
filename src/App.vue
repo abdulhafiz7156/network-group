4b8f4c7eb4a15fb4adcf4971b7eed5fea9da5772
abdulhafiz7156 <xxafiz69@gmail.com>
2024-07-18 19:55:03 +0500

    try to fix img bug 5

--- src/App.vue
+++ src/App.vue
@@ -294,7 +294,7 @@ export default {
     };
 
     const getPic = (index) => {
-      return 'https://network-group.vercel.app/' + 'src/assets/img/' + index;
+      return 'https://network-group.vercel.app/' + 'assets/img/' + index;
     }
 
     const animateCount = (element, endValue) => {
